c83c2c89b5750e43a177ed8003cad07240e286ec
Jhonny Vargas <jhonny.vargas.arias@gmail.com>
2024-10-27 01:06:01 -0600

    Add "database" and update endpoint with new pattern

--- src/data/reviews/types.ts
+++ src/data/reviews/types.ts
@@ -1,8 +1,10 @@
-type Review = {
+export type Review = {
   id: number;
   rating: number;
   review: string | null;
   author: string;
 };
 
-export default Review;
+export type ReviewData = {
+  reviews: Review[];
+};
--- src/pages/api/reviews.ts
+++ src/pages/api/reviews.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import data from '@/data/reviewsData';
-import Review from '@/data/Review';
+
+import { Review } from '@/data/reviews/types';
+import { getReviews } from '@/data/reviews';
 
 const DEFAULT_PAGE_NUMBER = 1;
 const DEFAULT_PAGE_SIZE = 2;
@@ -28,20 +29,27 @@ const parseNumberInput = (input: any) => {
   }
 };
 
-export default function handler(
+type Meta = {
+  page: number;
+  pageSize: number;
+  total: number;
+};
+
+type ReviewResponse = {
+  data: Review[];
+  meta: Meta;
+};
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Review[]>,
+  res: NextApiResponse<ReviewResponse>,
 ) {
   if (req.method === 'GET') {
     const page = parseNumberInput(req.query.page) || DEFAULT_PAGE_NUMBER;
     const pageSize = setPageSize(parseNumberInput(req.query.pageSize));
 
-    const startIndex = (page - 1) * pageSize;
-    const endIndex = page * pageSize;
+    const dataset = await getReviews(page, pageSize);
 
-    const dataset = data.filter(
-      (r: Review) => r.id > startIndex && r.id <= endIndex,
-    );
     res.status(200).json(dataset);
   } else {
     res.status(404);
